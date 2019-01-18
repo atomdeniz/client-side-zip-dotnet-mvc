40a5c977186204dfa89eef3ae282ea81de523113
atomdeniz <atom_deniz@hotmail.com>
2019-01-18 14:53:27 +0300

    first commit

---
+++ zip/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using System.Text;
+using Shared;
+
+namespace zip.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Save()
+        {
+            foreach (string file in Request.Files)
+            {
+                var FileDataContent = Request.Files[file];
+                if (FileDataContent != null && FileDataContent.ContentLength > 0)
+                {
+                    // take the input stream, and save it to a temp folder using
+                    // the original file.part name posted
+                    var stream = FileDataContent.InputStream;
+                    var fileName = Path.GetFileName(FileDataContent.FileName);
+                    var UploadPath = Server.MapPath("~/App_Data/uploads");
+                    Directory.CreateDirectory(UploadPath);
+                    string path = Path.Combine(UploadPath, fileName);
+                    try
+                    {
+                        if (System.IO.File.Exists(path))
+                            System.IO.File.Delete(path);
+                        using (var fileStream = System.IO.File.Create(path))
+                        {
+                            stream.CopyTo(fileStream);
+                        }
+                        // Once the file part is saved, see if we have enough to merge it
+                        Shared.Utils UT = new Shared.Utils();
+                        UT.MergeFile(path);
+                    }
+                    catch (IOException ex)  
+                    {
+                        // handle
+                    }
+                }
+            }
+           
+            return View();
+        }
+   
+
+    }
+}
